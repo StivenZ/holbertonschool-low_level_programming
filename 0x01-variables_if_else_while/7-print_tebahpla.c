a473c215d70987bf201552ae4432d2d9174afa9e
Stiven Espinosa <1806@holbertonschool.com>
2020-06-12 21:41:56 +0000

    Prints reverse alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet inversed
+ * Return: 0
+*/
+int main(void)
+{
+char i;
+	for (i = 122; i >= 97; i--)
+	{
+	putchar(i);
+	}
+putchar('\n');
+return(0);
+}
