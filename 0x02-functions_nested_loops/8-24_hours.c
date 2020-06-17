ddf7f6cd9b6cfe3fe8d48d6589f21b70d54274ca
Stiven Espinosa <1806@holbertonschool.com>
2020-06-17 05:24:31 +0000

    Actually prints seconds, not minutes

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,50 +1,24 @@
 #include "holberton.h"
 /**
- * jack_bauer - prints minutes in 24 hours, but they're actually every second, not every minute, other way it would be s
- * shorter
+ * jack_bauer - prints minutes in 24 hours,
+ * but they're actually every second, not every minute,
+ * other way it would be shorter
  * Return: whatever
- * @i, j, k, l: secods & minutes
+ * @i, j: secods & minutes
 */
 void jack_bauer(void)
 {
-int i, j, k, l;
-	for (i = 0; i <= 2; i++)
+int i, j;
+	for (i = 0; i < 24; i++)
 	{
-	if (i < 2)
-	{
-		for (j = 0; j <= 9; j++)
+		for (j = 0; j < 60; j++)
 		{
-			for (k = 0; k <= 5; k++)
-			{
-				for (l = 0; l <= 9; l++)
-				{
-					_putchar(i + '0');
-					_putchar(j + '0');
-                                        _putchar(':');
-					_putchar(k + '0');
-					_putchar(l + '0');
-					_putchar('\n');
-				}
-			}
+		_putchar(i / 10 + '0');
+		_putchar(i % 10 + '0');
+		_putchar(':');
+		_putchar(j / 10 + '0');
+		_putchar(j % 10 + '0');
+		_putchar('\n');
 		}
 	}
-	else
-	{
-	for (j = 0; j <= 3; j++)
-	{
-		for (k = 0; k <= 5; k++)
-		{
-			for (l = 0; l <= 9; l++)
-			{
-				_putchar(i + '0');
-				_putchar(j + '0');
-				_putchar(':');
-				_putchar(k + '0');
-				_putchar(l + '0');
-				_putchar('\n');
-			}
-		}
-	}
-	}
-}
 }
