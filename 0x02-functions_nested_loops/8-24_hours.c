65f013daa82bbc04b9c70c2dcc03dc5fdb824aae
Stiven Espinosa <1806@holbertonschool.com>
2020-06-17 03:01:01 +0000

    prints not minutes but seconds

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,7 +10,9 @@ void jack_bauer(void)
 int i, j, k, l;
 	for (i = 0; i <= 2; i++)
 	{
-		for (j = 0; j <= 3; j++)
+	if (i < 2)
+	{
+		for (j = 0; j <= 9; j++)
 		{
 			for (k = 0; k <= 5; k++)
 			{
@@ -26,4 +28,23 @@ int i, j, k, l;
 			}
 		}
 	}
+	else
+	{
+	for (j = 0; j <= 3; j++)
+	{
+		for (k = 0; k <= 5; k++)
+		{
+			for (l = 0; l <= 9; l++)
+			{
+				_putchar(i + '0');
+				_putchar(j + '0');
+				_putchar(':');
+				_putchar(k + '0');
+				_putchar(l + '0');
+				_putchar('\n');
+			}
+		}
+	}
+	}
+}
 }
