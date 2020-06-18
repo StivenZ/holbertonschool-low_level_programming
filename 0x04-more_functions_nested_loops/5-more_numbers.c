e800297a4cb7b00ece2f7a5944dc3dd5009e361b
Stiven Espinosa <1806@holbertonschool.com>
2020-06-18 22:29:42 +0000

    Prints even more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * more_numbers - prints more numbers
+ * i: parameter
+ * j: parameter
+*/
+void more_numbers(void)
+{
+int i, j;
+j = 0;
+	for (i = 0; i <= 9; i++)
+	{
+		while (j < 15)
+		{
+			if (j > 9)
+			{
+				_putchar(1 + '0');
+			}
+			_putchar((j % 10) + '0');
+			j++;
+		}
+	_putchar('\n');
+	}
+}
