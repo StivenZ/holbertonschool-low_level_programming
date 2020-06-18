8e96c0614eee368d0fe6dfdf1122c669d75f988a
Stiven Espinosa <1806@holbertonschool.com>
2020-06-18 21:55:19 +0000

    Prints most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,9 +8,12 @@ void print_most_numbers(void)
 int i;
 
 i = 0;
-	while (i != 2 && i != 4 && i < 10)
+	while (i < 10)
 	{
-		_putchar(i + '0');
+		if (i != 2 && i != 4)
+		{
+			_putchar(i + '0');
+		}
 		i++;
 	}
 	_putchar('\n');
