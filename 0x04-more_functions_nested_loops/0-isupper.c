f86a5eb5d2b68083b1ec4c9c7a93caab5eb4d284
Stiven Espinosa <1806@holbertonschool.com>
2020-06-18 19:31:51 +0000

    Check if char is uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * _islower - checks lower case characters
+ * @c: character
+ * Return: nothing
+*/
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+	return (1);
+	}
+	return (0);
+
+}
