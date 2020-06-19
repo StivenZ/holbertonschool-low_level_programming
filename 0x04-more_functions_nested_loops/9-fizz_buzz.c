b530f648ecd674ae61086802c29fd4f04eaa9fb5
Stiven Espinosa <1806@holbertonschool.com>
2020-06-19 03:11:26 +0000

    Prints fizz, buzz, both, or number

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * main - exectes fizz
+*/
+int main (void)
+{
+	_fizzbuzz();
+	return (0);
+}
+
+/**
+ * _fizzbuzz - prints numbers and fizz-buzz
+ * i: parameter
+*/
+void _fizzbuzz(void)
+{
+int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 3 == 0) && !(i % 5 == 0))
+		{
+			printf("Fizz ");
+		}
+		else if ((i % 5 == 0) && !(i % 3 == 0))
+		{
+			if (i != 100)
+			{
+				printf("Buzz ");
+			}
+			else
+			{
+				printf("Buzz\n");
+			}
+		}
+		else if ((i % 5 == 0) && (i % 3 == 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+}
