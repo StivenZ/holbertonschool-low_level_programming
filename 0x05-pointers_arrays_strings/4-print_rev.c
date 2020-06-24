934898d9412771664e6d0b2a3466353935c81e32
Stiven Espinosa <1806@holbertonschool.com>
2020-06-24 00:07:54 +0000

    Prints reverse string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+ * print_rev - prints a reverse string
+ * @s: string parameter
+*/
+void print_rev(char *s)
+{
+int i;
+int j;
+i = 0;
+
+/** Character count
+*/
+	while (s[i] != '\0')
+	{
+	i++;
+	}
+/** Reverse print
+*/
+j = i;
+	while(j >= 0)
+	{
+	_putchar(s[j]);
+	j--;
+	}
+_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -7,3 +7,5 @@ int _putchar(char c);
 int _strlen(char *s);
 
 void _puts(char *str);
+
+void print_rev(char *s);
