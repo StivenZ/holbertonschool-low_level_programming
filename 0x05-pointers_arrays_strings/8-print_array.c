a0a88075f8253419a276f797c5c78b482fb7d77d
Stiven Espinosa <1806@holbertonschool.com>
2020-06-25 04:39:20 +0000

    Prints n array elements

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -3,7 +3,7 @@
 /**
  * print_array - prints n elements of an array
  * @a: pointer parameter
- * n: n elements to be printed
+ * @n: n elements to be printed
  * i: parameter
 */
 void print_array(int *a, int n)
