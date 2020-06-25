1478f7c73ceb2707863f4996d9ba200543b00b13
Stiven Espinosa <1806@holbertonschool.com>
2020-06-25 03:42:21 +0000

    Prints half a string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -3,35 +3,34 @@
  * puts_half - prints half the string
  * @str: string parameter
  * i: parameter
- * j: parameter
+ * n: parameter
 */
 void puts_half(char *str)
 {
-int i, j;
+int i, n;
 i = 0;
 	while (str[i] != '\0')
 	{
 	i++;
 	}
-	i--;
 
 	if (i % 2 == 0)
 	{
-	j = i / 2;
-		while (str[j] != '\0')
+	n = (i / 2);
+		while (str[n] != '\0')
 		{
-		_putchar(str[j]);
-		j++;
+		_putchar(str[n]);
+		n++;
 		}
 	}
 
 	else if (i % 2 != 0)
 	{
-	j = (i - 1) / 2;
-		while (str[j] != '\0')
+	n = (i - 1) / 2;
+		while (str[n] != '\0')
 		{
-		_putchar(str[j]);
-		j++;
+		_putchar(str[n]);
+		n++;
 		}
 	}
 _putchar('\n');
