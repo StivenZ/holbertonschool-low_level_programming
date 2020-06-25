95ab45601c3d99568db78e10c39cf5760023c534
Stiven Espinosa <1806@holbertonschool.com>
2020-06-25 04:18:48 +0000

    Reverses string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,22 +7,20 @@
 */
 void rev_string(char *s)
 {
-	int j;
-	int i;
-	char cp[1000];
+	int i, j;
+	char t[1024];
 
 	i = 0;
+	j = 0;
 
-	while ((cp[i] = s[i]) != '\0')
+	while ((t[i] = s[i]) != '\0')
 	{
 		i++;
 	}
 	i--;
-	j = 0;
-
-	while ((s[j] = cp[i]) && (i > 0))
+	while ((s[j] = t[i]) && (i > 0))
 	{
-		j++;
 		i--;
+		j++;
 	}
 }
