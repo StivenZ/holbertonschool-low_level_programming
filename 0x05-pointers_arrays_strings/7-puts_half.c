55eb74abfa438372bd4f7df1f10ac03c3679ba1f
Stiven Espinosa <1806@holbertonschool.com>
2020-06-25 04:13:21 +0000

    Pints a inrr

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,27 +11,29 @@ int i, n;
 i = 0;
 	while (str[i] != '\0')
 	{
-	i++;
+		i++;
 	}
 
 	if (i % 2 == 0)
 	{
 	n = (i / 2);
+
 		while (str[n] != '\0')
 		{
-		_putchar(str[n]);
-		n++;
+			_putchar(str[n]);
+			n++;
 		}
 	}
 
 	else if (i % 2 != 0)
 	{
-	n = (i - 1) / 2;
-		while (str[n] != '\0')
-		{
-		_putchar(str[n]);
-		n++;
-		}
+		n = ((i - 1) / 2) + 1;
+
+			while (str[n] != '\0')
+			{
+				_putchar(str[n]);
+				n++;
+			}
 	}
 _putchar('\n');
 }
