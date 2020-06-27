838ebe69f5d3f370d4e39f373f9f2574865c5480
Stiven Espinosa <1806@holbertonschool.com>
2020-06-27 21:26:51 +0000

    Compares two strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+ * _strcmp - Compares two strings
+ * @s1: string to compare
+ * @s2: string compared to
+ * Return: integer
+*/
+int _strcmp(char *s1, char *s2)
+{
+	int i, j;
+
+	i = 0;
+	j = 0;
+
+	while (s1[i] != '\0')
+	{
+		i++;
+	}
+	i--;
+	while (s2[j] != '\0')
+	{
+		j++;
+	}
+	j--;
+
+	return (i - j);
+}
