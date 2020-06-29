47a9bfa090b034159b1fa512c1f635ce0f09bbe5
Stiven Espinosa <1806@holbertonschool.com>
2020-06-29 23:21:06 +0000

    Points to first character in string

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+/**
+ * *_strpbrk - locates the first occurrence of s
+ * inside *accept.
+ * @s: string to find
+ * @accept: where to seek
+ * Return : a pointer to s
+*/
+char *_strpbrk(char *s, char *accept)
+{
+
+	int j;
+
+	j = 0;
+
+	while (*s != '\0')
+	{
+
+		while (accept[j] != '\0')
+		{
+			if (accept[j] == *s)
+			{
+				return (s);
+			}
+			j++;
+		}
+	j = 0;
+	s++;
+	}
+
+	if (*s == '\0')
+	{
+		return ("NULL");
+	}
+
+	return (s);
+}
