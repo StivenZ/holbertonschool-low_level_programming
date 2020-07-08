02ff3e08d9935157da813e84237e69bf562f6118
Stiven Espinosa <1806@holbertonschool.com>
2020-07-08 04:53:19 +0000

    Prints function name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints function name
+ * @argc: arguments count
+ * @argv: array
+*/
+int main(int argc, char *argv[])
+{
+	if (argc == 1)
+	{
+	printf("%s\n", argv[0]);
+	}
+	return (0);
+}
