1a33bc719a29254fb9fbe353417343834a2b16df
stivenz <1806@holbertonschool.com>
2020-07-09 02:57:47 -0500

    Creates a pointer to a copy of a string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,40 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * *_strdup - creates a pointer to a new space in memory with
+ * a copy of the string given as a parameter
+ * @str: string and pointer
+ * Return: a pointer to a string
+*/
+char *_strdup(char *str)
+{
+	if (str[0] == '\0')
+	{
+		return (NULL);
+	}
+
+	int i = 0;
+	char *du;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+
+	du = (char *)malloc(sizeof(char) * i);
+
+	if (!du)
+	{
+		return (NULL);
+	}
+
+	i = 0;
+
+	while (str[i] != '\0')
+	{
+		du[i] = str[i];
+		i++;
+	}
+	return (du);
+}
