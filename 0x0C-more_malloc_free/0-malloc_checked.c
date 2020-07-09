a7cb392a932819ac8d9e636a6a84427ccb3255b5
stivenz <1806@holbertonschool.com>
2020-07-09 16:15:41 -0500

    Returns a pointer

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * *malloc_checked - returns a pointer to the allocated memory
+ * @b: memory amount
+ * Return: pointer
+*/
+void *malloc_checked(unsigned int b)
+{
+	void *s;
+
+	s = malloc(b);
+
+	if (!s)
+	{
+		exit(98);
+	}
+	return (s);
+}
