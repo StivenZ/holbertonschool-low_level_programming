1315f4d90e141aa7932675f1cff6e94697f7f2f7
stivenz <1806@holbertonschool.com>
2020-07-09 00:04:58 -0500

    Adds numbers submited

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main - adds two numbers
+ * @argc: arguments count
+ * @argv: arguments array
+ * Return: 0
+*/
+int main(int argc, char **argv)
+{
+	int i, j, k;
+
+	j = 0;
+	i = 1;
+	k = 0;
+
+	if (argc < 2)
+	{
+		printf("0\n");
+	}
+	if (argc > 2)
+	{
+		while (i < argc)
+		{
+			while (isdigit(argv[i][k]) != 0)
+			{
+				if (isdigit(*argv[k]) == 0)
+				{
+					printf("Error\n");
+					return (1);
+				}
+				k++;
+			}
+			j = j + atoi(argv[i]);
+			i++;
+		}
+		printf("%d\n", j);
+	}
+	return (0);
+}
