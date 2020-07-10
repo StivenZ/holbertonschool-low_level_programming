d3b1b04951bbd2157fb7fefe6dd7760d78c2077e
Stiven Espinosa <1806@holbertonschool.com>
2020-07-10 03:16:20 +0000

    Concatenates 2 strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,5 +1,21 @@
 #include "holberton.h"
 /**
+ * sizes - determines sizes of strings and turns to empty string
+ * @a: string
+ * Return: string size
+*/
+int sizes(char *a)
+{
+	unsigned int i = 0;
+
+	while (a[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
  * *string_nconcat - concatenates two strings
  * @s1: string 1
  * @s2: string 2
@@ -15,48 +31,27 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	bs1 = 0;
 	bs2 = 0;
 
-	while (s1[bs1] != '\0')
-	{
-		bs1++;
-	}
-
-	while (s2[bs2] != '\0')
-	{
-		bs2++;
-	}
+	bs1 = sizes(s1);
+	bs2 = sizes(s2);
 
 	if (n > bs2)
-	{
 		n = bs2;
-	}
 
 	if (!s1)
-	{
 		s1 = '\0';
-	}
 	if (!s2)
-	{
 		s2 = '\0';
-	}
-
 	bytes = (bs1 + n);
-
 	p = malloc(bytes);
-
 	if (!p)
-	{
 		return (NULL);
-	}
-
 	while (i < bs1)
 	{
 		p[i] = s1[i];
 		i++;
 	}
-
 	j = i;
 	i = 0;
-
 	while (j < bytes)
 	{
 		p[j] = s2[i];
