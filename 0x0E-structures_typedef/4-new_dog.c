e6ac263c6e0ae8099afd04d9ce4531c2a6a7a468
Stiven Espinosa <1806@holbertonschool.com>
2020-07-14 23:52:54 +0000

    Creates a new dog data

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,8 +12,11 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	new = malloc(sizeof(dog_t));
 
-	if (new == NULL)
+	if (!new)
+	{
+		free(new);
 		return (NULL);
+	}
 
 	(*new).name = name;
 	(*new).age = age;
