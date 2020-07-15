0542aac6263e3a358769dc03e7525ac67a888819
Stiven Espinosa <1806@holbertonschool.com>
2020-07-15 23:03:56 +0000

    Executes a function on an array

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - executes a parameter-like function
+ * @array: array of functions
+ * @size: size of the array
+ * @action: pointer to function
+*/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (!action)
+		return;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,11 @@
 #ifndef _HOLBERTON_H_
 #define _HOLBERTON_H_
+#include <stdlib.h>
+#include <stddef.h>
 
 void print_name(char *name, void (*f)(char *));
+
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 
 #endif /* _HOLBERTON_H_ */
