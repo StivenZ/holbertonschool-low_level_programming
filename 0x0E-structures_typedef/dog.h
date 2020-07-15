9b9bbb768bb37d76d9c93ad7543730470c5b2971
Stiven Espinosa <1806@holbertonschool.com>
2020-07-15 03:38:28 +0000

    Frees space

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,7 @@
 #ifndef _HOLBERTON_H_
 #define _HOLBERTON_H_
 #include <stdlib.h>
+#include <stdio.h>
 /**
  * struct dog - names and owns a dog
  * @name: dog's name
@@ -22,5 +23,7 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
 dog_t *new_dog(char *name, float age, char *owner);
+
+void free_dog(dog_t *d);
 
 #endif /* _HOLBERTON_H_ */
