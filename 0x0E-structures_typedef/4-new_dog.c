6bcc8e69a9d81e6f6f29572d12efe2fe9272e030
Stiven Espinosa <1806@holbertonschool.com>
2020-07-15 01:47:24 +0000

    Creates a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,4 +1,34 @@
 #include "dog.h"
+#include <stdlib.h>
+/**
+ * *_strcpy - copies a string
+ * @dest: parameter
+ * @src: parameter
+ * Return: string
+*/
+char *_strcpy(char *dest, char *src)
+{
+int i, j;
+
+i = 0;
+
+	while (src[i++] != '\0')
+	{; }
+
+j = 0;
+
+	while (j <= i)
+	{
+		dest[j] = src[j];
+		if (dest[j] == '\0')
+		{
+			break;
+		}
+	j++;
+	}
+return (dest);
+}
+
 /**
  * *new_dog - creates a new dog
  * @name: name of dog
@@ -9,6 +39,20 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new;
+	char *ncpy, *ocpy;
+	int i, j;
+
+	i = 0;
+	j = 0;
+	while (name[i++])
+	{; }
+	ncpy = malloc(sizeof(char) * i);
+	_strcpy(ncpy, name);
+
+	while (name[j++])
+	{; }
+	ocpy = malloc(sizeof(char) * j);
+	_strcpy(ocpy, owner);
 
 	new = malloc(sizeof(dog_t));
 
