dc6dbc096fe5bb57c5d30368af2b6958f88a38ab
Stiven Espinosa <1806@holbertonschool.com>
2020-07-17 06:14:02 +0000

    Calculator

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -19,18 +19,19 @@ int main(int argc, char **argv)
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
 
-	if ((*argv[2] == '/' || *argv[2] == '%') && atoi(argv[3]) == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
-
 	if (*argv[2] != '/' && *argv[2] != '+' && *argv[2] != '-' && *argv[2] != '*'
 		&& *argv[2] != '%')
 	{
 		printf("Error\n");
 		exit(99);
 	}
+
+	if ((*argv[2] == '/' || *argv[2] == '%') && atoi(argv[3]) == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
 	c = get_op_func(argv[2])(a, b);
 
 	printf("%d\n", c);
