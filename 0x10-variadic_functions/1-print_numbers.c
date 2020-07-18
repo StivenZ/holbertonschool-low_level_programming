e9d9eff2e063201f6c7bb63c946a86a9bffd48df
Stiven Espinosa <1806@holbertonschool.com>
2020-07-18 20:05:57 +0000

    Prints numbers from arguments

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,27 @@
+#include "variadic_functions.h"
+/**
+ * print_numbers - prints strings
+ * @separator: separates printed strings
+ * @n: number of arguments
+*/
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list init;
+	unsigned int i;
+
+	va_start(init, n);
+
+	if (!*separator)
+		return;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(init, unsigned int));
+		if (i < n - 1)
+		{
+			printf("%s", separator);
+		}
+		else
+			printf("\n");
+	}
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,7 +1,10 @@
 #ifndef _HOLBERTON_H_
 #define _HOLBERTON_H_
-#include "stdarg.h"
+#include <stdarg.h>
+#include <stdio.h>
 
 int sum_them_all(const unsigned int n, ...);
+
+void print_numbers(const char *separator, const unsigned int n, ...);
 
 #endif /* _HOLBERTON_H_ */
