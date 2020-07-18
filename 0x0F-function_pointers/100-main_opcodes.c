59327fc51fa5388b9fd769c98cde089a72ece0d6
Stiven Espinosa <1806@holbertonschool.com>
2020-07-18 04:55:27 +0000

    Opcode

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints opcode
+ * @argc: arguments count
+ * @argv: arguments array
+ * Return: 0
+*/
+
+int main(int argc, char **argv)
+{
+
+	int bytes = atoi(argv[1]);
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	if (bytes < 0)
+	{
+		printf("Error");
+		exit(2);
+	}
+
+	return (0);
+}
