a2e860bf5ec02f71d2aca1e224b3c4715592bc1d
Stiven Espinosa <1806@holbertonschool.com>
2020-07-18 20:25:00 +0000

    Prints numbers from arguments

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -11,6 +11,9 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	va_start(init, n);
 
+	if (n == 0)
+		return;
+
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(init, unsigned int));
