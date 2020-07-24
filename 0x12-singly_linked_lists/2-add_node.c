b4b9976322755757404fad48662b943360d01fa4
Stiven Espinosa <1806@holbertonschool.com>
2020-07-24 04:55:27 +0000

    Adds a new node to the beginning of the list

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+/**
+ * add_node - adds a node to the list
+ * @head: pointer to the head of the linked list
+ * @str: content of the node to be copied
+ * Returns: a pointer to a new node
+*/
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node;
+	char *strcopy;
+	int i = 0;
+	new_node = malloc(sizeof(list_t));
+
+	if (!new_node)
+		return (NULL);
+
+	while (str[i])
+	{
+		i++;
+	}
+
+	strcopy = malloc((sizeof(char)) * (i));
+	if (!strcopy)
+		return (NULL);
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		strcopy[i] = str[i];
+		i++;
+	}
+
+	new_node->len = (i);
+	new_node->str = strcopy;
+	new_node->next = *head;
+	*head = new_node;
+
+	return (new_node);
+}
