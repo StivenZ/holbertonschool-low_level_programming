34e4b255479c7d4c2f5f37246b16b7eb07b2dd0d
Stiven Espinosa <1806@holbertonschool.com>
2020-07-24 06:30:22 +0000

    Adds a new node to the beginning of the list

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,5 +1,21 @@
 #include "lists.h"
 /**
+ * length - Calculates string length
+ * @str: string pointer
+ * Return: i: length
+ */
+int length(const char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
  * add_node - adds a node to the list
  * @head: pointer to the head of the linked list
  * @str: content of the node to be copied
@@ -9,26 +25,22 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
 	char *strcopy;
-	unsigned int i = 0;
+	int i = 0;
 
 	new_node = malloc(sizeof(list_t));
 
 	if (head == NULL || !str || !new_node)
-		return (NULL);
-
-	while (str[i] != '\0')
 	{
-		i++;
+		return (NULL);
 	}
 
-	strcopy = malloc((sizeof(char)) * (i + 1));
+	strcopy = malloc((sizeof(char)) * (length(str) + 1));
 
 	if (strcopy == NULL)
 	{
 		return (NULL);
 	}
 
-	i = 0;
 	while (str[i] != '\0')
 	{
 		strcopy[i] = str[i];
