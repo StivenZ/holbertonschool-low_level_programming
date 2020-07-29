7d8b762d66eb46aee778b2933302f66cab220a75
Stiven Espinosa <1806@holbertonschool.com>
2020-07-29 03:59:50 +0000

    Adding condition when next position exists

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,7 +15,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 
 	temp = *head;
-	i = 0;
+	i = 1;
 
 	new_node = malloc(sizeof(listint_t));
 
@@ -32,16 +32,19 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	while (temp)
 	{
-		if (i + 1 == idx)
+		if (i == idx)
 		{
 			new_node->n = n;
 			new_node->next = temp->next;
 			temp->next = new_node;
 			break;
 		}
-		temp = temp->next;
-		i++;
-		if (temp == NULL)
+		if (temp->next)
+		{
+			temp = temp->next;
+			i++;
+		}
+		else
 			return (NULL);
 	}
 
