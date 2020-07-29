66bedf3945dda9ede65bde6aac057f6dc30d96b2
Stiven Espinosa <1806@holbertonschool.com>
2020-07-29 04:59:58 +0000

    Deketes node at index

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,45 @@
+#include "lists.h"
+/**
+ * delete_nodeint_at_index - deletes a node at the index
+ * @head: pointer to the head of the list
+ * @index: position where the node is going to be deleted
+ * Return: 1 if succeded, -1 if failed
+*/
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *temp, *temp1;
+	unsigned int i;
+
+	if (!head)
+		return (-1);
+
+	temp = temp1 = *head;
+
+	i = 0;
+
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		free(temp);
+		return (1);
+		
+	}
+	while (temp)
+	{
+		if (i + 1 == index)
+		{
+			temp = temp->next;
+			temp1 = temp->next;
+			free(temp);
+			return (1);
+		}
+		if (temp->next)
+		{
+			temp = temp1 = temp->next;
+			i++;
+		}
+		else
+			return (-1);
+	}
+	return (-1);
+}
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -37,4 +37,8 @@ int sum_listint(listint_t *head);
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 
+int delete_nodeint_at_index(listint_t **head, unsigned int index);
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index);
+
 #endif /* _HOLBERTON_H_ */
