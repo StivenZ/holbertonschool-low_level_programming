32f37325a1fcf917624f6f643b76b32b134f760d
Stiven Espinosa <1806@holbertonschool.com>
2020-07-29 19:03:58 +0000

    Adding case head is NULL

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -15,7 +15,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	temp = temp1 = *head;
 
-	i = 0;
+	i = 1;
 
 	if (index == 0)
 	{
@@ -25,7 +25,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	}
 	while (temp)
 	{
-		if (i + 1 == index)
+		if (i == index && (temp->next) != NULL)
 		{
 			temp = temp->next;
 			temp1 = temp->next;
