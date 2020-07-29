e6919c84b6a8202894d18c0717d435b9740d915b
Stiven Espinosa <1806@holbertonschool.com>
2020-07-29 03:30:38 +0000

    Fixing case when index is 0

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -22,6 +22,14 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (!new_node)
 		return (NULL);
 
+	if (idx == 0)
+	{
+		new_node->n = n;
+		new_node->next = temp;
+		temp->next = new_node;
+		return (new_node);
+	}
+
 	while (temp)
 	{
 		if (i + 1 == idx)
