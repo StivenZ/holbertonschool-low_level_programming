7ac63e6ef4b777a239f363ad0549117f0bf1a8a3
Stiven Espinosa <1806@holbertonschool.com>
2020-07-29 04:53:56 +0000

    Fixing case index is 0

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -23,8 +23,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (idx == 0)
 	{
 		new_node->n = n;
-		new_node->next = temp;
-		temp->next = new_node;
+		new_node->next = *head;
+		*head = new_node;
 		return (new_node);
 	}
 
