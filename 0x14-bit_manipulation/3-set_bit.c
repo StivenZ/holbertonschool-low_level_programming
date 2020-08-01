934f76ec154dcef3b52e1dd587f9c9d655bd5fad
Stiven Espinosa <1806@holbertonschool.com>
2020-08-01 21:14:36 +0000

    Sets a bit in a given position to 1

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -12,7 +12,7 @@ int set_bit(unsigned long int *n, unsigned int index)
 
 	if (index < (sizeof(unsigned long int) * 8))
 	{
-		(*n) >> index = ~((*n) >> index);
+		*n |= 1 << index;
 		return (1);
 	}
 	else
