57a3384a7cdf2685d7ca84501f22bf571d2dd16b
Stiven Espinosa <1806@holbertonschool.com>
2020-08-10 18:55:54 +0000

    Fixed case text content null

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -15,7 +15,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (text_content == NULL)
 	{
-		return (-1);
+		return (1);
 	}
 
 	while (text_content[i] != '\0')
@@ -23,7 +23,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		i++;
 	}
 
-	fd = open(filename, O_APPEND | O_WRONLY, 0600);
+	fd = open(filename, O_APPEND | O_WRONLY);
 
 	if (fd == -1)
 	{
