01c1ff75353dd3345c1ddb43aa21c8263918011e
stivenz <1806@holbertonschool.com>
2020-09-14 11:13:16 -0500

    prints lements

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+/**
+ * print_dlistint - prints elements of a linked list
+ * @h: pointer to the list
+ * Return: number of nodes
+*/
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	const dlistint_t *tmp;
+	size_t i;
+
+	i = 0;
+	tmp = h;
+
+	if (h == NULL)
+	{
+		return (EXIT_FAILURE);
+	}
+
+	while (tmp != NULL)
+	{
+		printf("%d\n", tmp->n);
+		tmp = tmp->next;
+		i++;
+	}
+
+	return (i);
+}
