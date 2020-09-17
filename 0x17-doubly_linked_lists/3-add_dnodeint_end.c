c271f133bff028687a912be39a35e06aaf8330d0
stivenz <1806@holbertonschool.com>
2020-09-17 12:17:45 -0500

    Fixed style

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -7,28 +7,28 @@
 */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-    dlistint_t *new_node;
-    dlistint_t *tmp;
+	dlistint_t *new_node;
+	dlistint_t *tmp;
 
-    tmp = *head;
-    new_node = malloc(sizeof(dlistint_t));
-    if (!new_node)
-        return (NULL);
+	tmp = *head;
+	new_node = malloc(sizeof(dlistint_t));
+	if (!new_node)
+		return (NULL);
 
-    new_node->n = n;
-    new_node->next = NULL;
-    if (!(*head))
-    {
-        new_node->prev = *head;
-        (*head) = new_node;
-        return (new_node);
-    }
+	new_node->n = n;
+	new_node->next = NULL;
+	if (!(*head))
+	{
+		new_node->prev = *head;
+		(*head) = new_node;
+		return (new_node);
+	}
 
-    while (tmp->next)
-    {
-        tmp = tmp->next;
-    }
-    tmp->next = new_node;
-    new_node->prev = tmp;
-    return (new_node);
+	while (tmp->next)
+	{
+		tmp = tmp->next;
+	}
+	tmp->next = new_node;
+	new_node->prev = tmp;
+	return (new_node);
 }
