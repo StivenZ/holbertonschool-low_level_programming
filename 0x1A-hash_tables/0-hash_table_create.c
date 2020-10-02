d63d7539e1f624bf0d18118ac0f6b7e5b89af809
stivenz <1806@holbertonschool.com>
2020-10-02 22:23:34 +0000

    Fixed condition

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,7 +8,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_hash_table;
 
-	if (!size || size == 0)
+	if (size == 0)
 		return (NULL);
 
 	new_hash_table = malloc(sizeof(hash_table_t));
