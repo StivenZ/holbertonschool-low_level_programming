07488a70cdaa29f4e846d1359172f3639a907d6e
stivenz <1806@holbertonschool.com>
2020-10-02 21:58:45 +0000

    INItializaed structure

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,8 +11,12 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (!size || size == 0)
 		return (NULL);
 
-	new_hash_table = calloc(size, sizeof(hash_table_t));
+	new_hash_table = malloc(sizeof(hash_table_t));
 	if (!new_hash_table)
+		return (NULL);
+
+	new_hash_table->array = calloc(size, sizeof(hash_node_t));
+	if (!(new_hash_table->array))
 		return (NULL);
 
 	new_hash_table->size = size;
