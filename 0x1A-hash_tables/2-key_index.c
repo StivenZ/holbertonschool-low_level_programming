293c44bb6c4efd356464e8a97d9fdb761b83ec91
stivenz <1806@holbertonschool.com>
2020-10-03 02:52:29 +0000

    Gets indesx

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,11 @@
+#include "hash_tables.h"
+/**
+ * key_index - returns the index of a key
+ * @key: key of the item of the hash table
+ * @size: size of the array of the hash table
+ * Return: index of the key
+*/
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb2(key) % size);
+}
