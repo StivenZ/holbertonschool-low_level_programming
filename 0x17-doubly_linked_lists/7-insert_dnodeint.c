cf8a9cd32051590f6aed4cffb04e58e552e58a61
stivenz <1806@holbertonschool.com>
2020-12-02 22:27:00 -0500

    Fixed add node at index

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,48 @@
+#include "lists.h"
+/**
+ * insert_dnodeint_at_index - add node at given index
+ * @h: pointer to head of linked list
+ * @idx: index
+ * @n: data from linked list
+ * Return: new node
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	unsigned int a = 0;
+	dlistint_t *new_node = malloc(sizeof(dlistint_t));
+	dlistint_t *aux = *h;
+
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	if (*h == NULL && idx == 0)
+		return (add_dnodeint(h, n));
+	while (aux != NULL)
+	{
+		if (idx == a)
+		{
+			if (idx == 0)
+			{
+				return (add_dnodeint(h, n));
+			}
+			else
+			{
+				new_node->n = n;
+				new_node->next = aux;
+				new_node->prev = aux->prev;
+				aux->prev = new_node;
+				aux = new_node->prev;
+				aux->next = new_node;
+				return (new_node);
+			}
+		}
+		a++;
+		aux = aux->next;
+	}
+	if (idx == a)
+	{
+		return (add_dnodeint_end(h, n));
+	}
+	return (NULL);
+}
