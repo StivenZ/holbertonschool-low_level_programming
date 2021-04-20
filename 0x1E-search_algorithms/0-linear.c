9222dd4153582809a1153bf53831e4e6bea0331c
stivenz <1806@holbertonschool.com>
2021-04-20 14:15:19 -0500

    Added comments

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,4 +1,11 @@
 #include "search_algos.h"
+/**
+ * linear_search - linear search algorithm.
+ * @array: pointer to int array.
+ * @size: size of the array.
+ * @value: value to find
+ * Return: Index of value found, or -1 if failure.
+*/
 
 int linear_search(int *array, size_t size, int value)
 {
