e964f43da6694fe6a8f2535c5859053b6f767713
stivenz <1806@holbertonschool.com>
2021-04-20 14:32:13 -0500

    Converted indentation to tabs

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -6,20 +6,19 @@
  * @value: value to find
  * Return: Index of value found, or -1 if failure.
 */
-
 int linear_search(int *array, size_t size, int value)
 {
-    size_t i = 0;
+	size_t i = 0;
 
-    if (array == NULL)
-        return (-1);
+	if (array == NULL)
+		return (-1);
 
-    while (i < size)
-    {
-        printf("Value checked array[%zu] = [%d]\n", i, array[i]);
-        if (value == array[i])
-            return (i);
-        i++;
-    }
-    return (-1);
+	while (i < size)
+	{
+		printf("Value checked array[%zu] = [%d]\n", i, array[i]);
+		if (value == array[i])
+			return (i);
+		i++;
+	}
+	return (-1);
 }
